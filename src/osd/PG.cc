83f506edded528ddbeb1fae0ff16b79bb60973be
J. Eric Ivancich <ivancich@redhat.com>
2016-06-17 12:40:30 -0400

    Make constructor for PGQueueable explicit to make references to that specific constructor easier to find. Was difficult With the implicit constructor invokation plus use of make_pair.

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -1890,7 +1890,7 @@ void PG::take_op_map_waiters()
     if (op_must_wait_for_map(get_osdmap_with_maplock()->get_epoch(), *i)) {
       break;
     } else {
-      osd->op_wq.queue(make_pair(PGRef(this), *i));
+      osd->op_wq.queue(make_pair(PGRef(this), PGQueueable(*i)));
       waiting_for_map.erase(i++);
     }
   }
@@ -1911,7 +1911,7 @@ void PG::queue_op(OpRequestRef& op)
     return;
   }
   op->mark_queued_for_pg();
-  osd->op_wq.queue(make_pair(PGRef(this), op));
+  osd->op_wq.queue(make_pair(PGRef(this), PGQueueable(op)));
   {
     // after queue() to include any locking costs
 #ifdef WITH_LTTNG
@@ -3257,7 +3257,7 @@ void PG::requeue_object_waiters(map<hobject_t, list<OpRequestRef>, hobject_t::Bi
 
 void PG::requeue_op(OpRequestRef op)
 {
-  osd->op_wq.queue_front(make_pair(PGRef(this), op));
+  osd->op_wq.queue_front(make_pair(PGRef(this), PGQueueable(op)));
 }
 
 void PG::requeue_ops(list<OpRequestRef> &ls)
@@ -3266,7 +3266,7 @@ void PG::requeue_ops(list<OpRequestRef> &ls)
   for (list<OpRequestRef>::reverse_iterator i = ls.rbegin();
        i != ls.rend();
        ++i) {
-    osd->op_wq.queue_front(make_pair(PGRef(this), *i));
+    osd->op_wq.queue_front(make_pair(PGRef(this), PGQueueable(*i)));
   }
   ls.clear();
 }
