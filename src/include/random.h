adb5240025ebc000190b9a63f027b1e58345b659
Jesse Williamson <jwilliamson@suse.de>
2017-05-18 06:11:13 -0700

    common: implement random number generator (following N3551)
    Adds new mini-library for random number generation using C++11's
    <random> header.
    
    Refer-to:
    https://isocpp.org/files/papers/n3551.pdf
    
    Signed-off-by: Jesse Williamson <jwilliamson@suse.de>

---
+++ src/include/random.h
@@ -0,0 +1,391 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2017 SUSE LINUX GmbH
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+*/
+
+#ifndef CEPH_RANDOM_H
+#define CEPH_RANDOM_H 1
+
+#include <mutex>
+#include <random>
+
+// Basic random number facility, adapted from N3551:
+namespace ceph {
+namespace util {
+
+inline namespace version_2017_0 {
+
+namespace detail {
+
+template <typename T>
+T& engine()
+{
+ thread_local T rng_engine;
+ return rng_engine;
+}
+
+template <typename MutexT, typename EngineT>
+void randomize_rng(const int seed, MutexT& m, EngineT& e)
+{
+ std::lock_guard<MutexT> lg(m);
+ e.seed(seed);
+}
+
+template <typename MutexT, typename EngineT>
+void randomize_rng(MutexT& m, EngineT& e)
+{
+ thread_local std::random_device rd;
+
+ std::lock_guard<MutexT> lg(m);
+ e.seed(rd());
+}
+
+template <typename MutexT, 
+          typename EngineT = std::default_random_engine>
+void randomize_rng(MutexT& m)
+{
+ randomize_rng(m, detail::engine<EngineT>());
+}
+
+template <typename MutexT, 
+          typename EngineT = std::default_random_engine>
+void randomize_rng(const int n, MutexT& m)
+{
+ std::lock_guard<MutexT> lg(m);
+ detail::engine<EngineT>().seed(n);
+}
+
+} // namespace detail
+
+namespace detail {
+
+template <typename NumberT,
+          typename MutexT,
+          typename DistributionT,
+          typename EngineT>
+NumberT generate_random_number(const NumberT min, const NumberT max,
+                               MutexT& m, EngineT& e)
+{
+ thread_local DistributionT d { min, max };
+
+ using param_type = typename DistributionT::param_type;
+
+ std::lock_guard<MutexT> lg(m);
+ return d(e, param_type { min, max });
+}
+
+template <typename NumberT,
+          typename MutexT,
+          typename DistributionT,
+          typename EngineT>
+NumberT generate_random_number(const NumberT min, const NumberT max,
+                               MutexT& m)
+{
+ return detail::generate_random_number<NumberT, MutexT, DistributionT, EngineT>
+         (min, max, m, detail::engine<EngineT>());
+}
+
+template <typename NumberT,
+          typename MutexT,
+          typename DistributionT,
+          typename EngineT>
+NumberT generate_random_number(const NumberT min, const NumberT max)
+{
+ thread_local std::mutex m;
+ return generate_random_number<DistributionT, EngineT>(min, max, m);
+}
+
+} // namespace detail
+
+template <typename EngineT = std::default_random_engine>
+void randomize_rng()
+{
+ thread_local std::mutex m;
+ detail::randomize_rng<decltype(m), EngineT>(m);
+}
+
+template <typename MutexT, typename EngineT = std::default_random_engine>
+void randomize_rng(MutexT& m)
+{
+ detail::randomize_rng<MutexT, EngineT>(m);
+}
+
+template <typename EngineT = std::default_random_engine>
+void randomize_rng(const int seed)
+{
+ thread_local std::mutex m;
+ detail::randomize_rng<decltype(m), EngineT>(seed, m);
+}
+
+template <typename MutexT,
+          typename EngineT = std::default_random_engine>
+void randomize_rng(const int seed, MutexT& m)
+{
+ detail::randomize_rng<MutexT, EngineT>(seed, m);
+}
+
+template <typename MutexT, typename EngineT>
+void randomize_rng(MutexT& m, EngineT& e)
+{
+ return detail::randomize_rng(m, e);
+}
+
+template <typename MutexT, typename EngineT>
+void randomize_rng(const int seed, MutexT& m, EngineT& e)
+{
+ return detail::randomize_rng(seed, m, e);
+}
+
+// Generate a random number between 0 and maxint:
+template <typename MutexT, typename EngineT,
+          int min = 0,
+          int max = std::numeric_limits<int>::max(), 
+          typename DistributionT = std::uniform_int_distribution<int>>
+int generate_random_number(MutexT& m, EngineT& e)
+{
+ return detail::generate_random_number<int, MutexT, DistributionT, EngineT>
+        (min, max, m, e);
+}
+
+template <typename MutexT,
+          int min = 0,
+          int max = std::numeric_limits<int>::max(), 
+          typename DistributionT = std::uniform_int_distribution<int>,
+          typename EngineT = std::default_random_engine>
+int generate_random_number(MutexT& m)
+{
+ return detail::generate_random_number<int, MutexT, DistributionT, EngineT> 
+         (min, max, m);
+}
+
+template <int min = 0,
+          int max = std::numeric_limits<int>::max(), 
+          typename DistributionT = std::uniform_int_distribution<int>,
+          typename EngineT = std::default_random_engine>
+int generate_random_number()
+{
+ thread_local std::mutex m;
+
+ return detail::generate_random_number<int, decltype(m), DistributionT, EngineT>
+        (min, max, m);
+}
+
+template <typename IntegerT, typename MutexT>
+IntegerT generate_random_number(const IntegerT min, const IntegerT max, MutexT& m,
+                                typename std::enable_if<std::is_integral<IntegerT>::value>::type* = nullptr)
+{
+ return detail::generate_random_number<IntegerT, decltype(m),
+                                       std::uniform_int_distribution<IntegerT>, 
+                                       std::default_random_engine>
+                                      (min, max, m); 
+}
+
+template <typename IntegerT>
+IntegerT generate_random_number(const IntegerT min, const IntegerT max,
+                                typename std::enable_if<std::is_integral<IntegerT>::value>::type* = nullptr)
+{
+ thread_local std::mutex m;
+ return generate_random_number(min, max, m); 
+}
+
+template <typename IntegerT, typename MutexT>
+int generate_random_number(const IntegerT max,
+                           MutexT& m,
+                           typename std::enable_if<std::is_integral<IntegerT>::value>::type* = nullptr)
+{
+ constexpr IntegerT zero = 0;   // no variable templates in C++11 :-(
+ return detail::generate_random_number<IntegerT, decltype(m),
+                                       std::uniform_int_distribution<IntegerT>,
+                                       std::default_random_engine>
+                                       (zero, max, m);
+}
+
+template <typename IntegerT, typename MutexT, typename EngineT>
+int generate_random_number(const IntegerT min, const IntegerT max,
+                           MutexT& m, EngineT& e,
+                           typename std::enable_if<std::is_integral<IntegerT>::value>::type* = nullptr)
+{
+ return detail::generate_random_number<IntegerT, MutexT,
+                                       std::uniform_int_distribution<IntegerT>,
+                                       EngineT>
+                                       (min, max, m, e);
+}
+
+template <typename IntegerT, typename MutexT, typename EngineT>
+int generate_random_number(const IntegerT max,
+                           MutexT& m, EngineT& e,
+                           typename std::enable_if<std::is_integral<IntegerT>::value>::type* = nullptr)
+{
+ constexpr IntegerT zero = 0;   // no variable templates in C++11 :-(
+ return generate_random_number(zero, max, m, e);
+}
+
+template <typename IntegerT>
+int generate_random_number(const IntegerT max,
+                           typename std::enable_if<std::is_integral<IntegerT>::value>::type* = nullptr)
+{
+ constexpr IntegerT zero = 0;   
+ return generate_random_number(zero, max);
+}
+
+template <typename RealT, typename MutexT>
+RealT generate_random_number(const RealT min, const RealT max, MutexT& m,
+                             typename std::enable_if<std::is_floating_point<RealT>::value>::type* = nullptr)
+{
+ return detail::generate_random_number<RealT, decltype(m),
+                                       std::uniform_real_distribution<RealT>, 
+                                       std::default_random_engine>
+                                      (min, max, m);
+}
+
+template <typename RealT>
+RealT generate_random_number(const RealT min, const RealT max, 
+                             typename std::enable_if<std::is_floating_point<RealT>::value>::type* = nullptr)
+{
+ thread_local std::mutex m;
+ return detail::generate_random_number<RealT, decltype(m),
+                                       std::uniform_real_distribution<RealT>, 
+                                       std::default_random_engine>
+                                      (min, max, m);
+}
+
+template <typename RealT, typename MutexT>
+RealT generate_random_number(const RealT max, MutexT& m,
+                             typename std::enable_if<std::is_floating_point<RealT>::value>::type* = nullptr)
+{
+ constexpr RealT zero = 0.0;
+ return generate_random_number(zero, max, m);
+}
+
+template <typename RealT, typename MutexT, typename EngineT>
+RealT generate_random_number(const RealT min, const RealT max, MutexT& m, EngineT& e,
+                             typename std::enable_if<std::is_floating_point<RealT>::value>::type* = nullptr)
+{
+ return detail::generate_random_number<RealT, MutexT, 
+                                       std::uniform_real_distribution<RealT>,
+                                       EngineT>
+        (min, max, m, e);
+
+}
+
+
+template <typename RealT, typename MutexT, typename EngineT>
+RealT generate_random_number(const RealT max, MutexT& m, EngineT& e,
+                             typename std::enable_if<std::is_floating_point<RealT>::value>::type* = nullptr)
+{
+ constexpr RealT zero = 0.0;
+ return generate_random_number(zero, max, m, e);
+}
+
+template <typename RealT>
+RealT generate_random_number(const RealT max,
+                             typename std::enable_if<std::is_floating_point<RealT>::value>::type* = nullptr)
+{
+ constexpr RealT zero = 0.0;
+
+ thread_local std::mutex m;
+
+ return generate_random_number(zero, max, m);
+}
+
+// Convenience functions:
+
+namespace detail {
+
+// Note that C++11 restricts us here because we can't get at the type of
+// lambda and there's limited return type deduction:
+template <typename IntT,
+          IntT Min, IntT Max, 
+          typename EngineT = std::default_random_engine>
+auto make_random_number_function() -> IntT(*)()
+{
+ return []() {
+            static std::mutex m;
+            static EngineT e;
+
+            return ceph::util::generate_random_number(Min, Max, m, e);
+        };
+}
+
+template <typename IntT,
+          IntT Min, IntT Max, IntT Seed, 
+          typename EngineT = std::default_random_engine>
+auto make_random_number_function() -> IntT(*)()
+{
+ return []() {
+            static std::mutex m;
+            static EngineT e(Seed);
+
+            return ceph::util::generate_random_number(Min, Max, m, e);
+        };
+}
+
+} // namespace detail
+
+template <int Min, int Max, 
+          typename EngineT = std::default_random_engine>
+auto make_random_number_function() -> int(*)()
+{
+ return detail::make_random_number_function<int, Min, Max, EngineT>();
+}
+
+template <int Min, int Max, int Seed,
+          typename EngineT = std::default_random_engine>
+auto make_random_number_function() -> int(*)()
+{
+ return detail::make_random_number_function<int, Min, Max, Seed, EngineT>();
+}
+
+// Function object:
+template <typename NumberT>
+class random_number_generator final
+{
+ std::mutex l;
+ std::random_device rd;
+ std::default_random_engine e;
+
+ public:
+ using number_type = NumberT;
+
+ public:
+ random_number_generator()
+ {
+    ceph::util::randomize_rng(l, e);
+ }
+
+ random_number_generator(const int seed)
+ {
+    ceph::util::randomize_rng(seed, l, e);
+ }
+
+ public:
+ random_number_generator(const random_number_generator&)            = delete;
+ random_number_generator& operator=(const random_number_generator&) = delete;
+
+ random_number_generator(random_number_generator&& rhs)
+  : e(std::move(rhs.e))
+ {}
+
+ public:
+ NumberT operator()()                                       { return generate_random_number(l, e); }
+ NumberT operator()(const NumberT max)                      { return generate_random_number(max, l, e); }
+ NumberT operator()(const NumberT min, const NumberT max)   { return generate_random_number(min, max, l, e); }
+
+ public:
+ void seed(const int n) { randomize_rng(n, l, e); }
+};
+
+} // inline namespace version_2017_0
+
+}} // namespace ceph::util
+
+#endif
---
+++ src/test/common/test_random.cc
@@ -0,0 +1,279 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2017 SUSE LINUX GmbH
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+*/
+
+#include <sstream>
+
+#include "include/random.h"
+
+#include "gtest/gtest.h"
+
+// Helper to see if calls compile with various types:
+template <typename T>
+T type_check_ok(const T min, const T max)
+{
+ return ceph::util::generate_random_number(min, max);
+}
+
+/* This mixes integer and real types freely, so it's not the greatest
+of tests, but should work casually for us: */
+template <typename X0, typename X1>
+bool none_equal(const X0 x0, const X1 x1)
+{
+ return x0 != x1;
+}
+
+template <typename X0, typename X1, typename ...XS>
+bool none_equal(const X0 x0, const X1 x1, const XS ...xs)
+{
+ return none_equal(x0, none_equal(x1, xs...));
+}
+
+// Mini-examples showing canonical usage:
+TEST(util, test_random_canonical)
+{
+ // Seed random number generation:
+ ceph::util::randomize_rng();
+
+ // Get a random int between 0 and max int:
+ auto a = ceph::util::generate_random_number();
+
+ // Get a random int between 0 and 20:
+ auto b = ceph::util::generate_random_number(20);
+
+ // Get a random int between 1 and 20:
+ auto c = ceph::util::generate_random_number(1, 20);
+
+ // Get a random float between 0.0 and 20.0:
+ auto d = ceph::util::generate_random_number(20.0);
+
+ // Get a random float between 0.001 and 0.991:
+ auto e = ceph::util::generate_random_number(0.001, 0.991);
+
+ // Make a random number function suitable for putting on its own thread:
+ auto gen_f = ceph::util::make_random_number_function<1, 20>();
+ auto x = gen_f();
+
+ // Make a function object RNG suitable for putting on its own thread:
+ auto gen_fo = ceph::util::random_number_generator<int>();
+ auto y = gen_fo();
+ gen_fo.seed(42);   // re-seed
+
+ // Mostly to swallow "unused variable" warnings:
+ ASSERT_EQ(true, none_equal(a, b, c, d, e, x, y));
+}
+
+TEST(util, test_random)
+{
+ /* The intent of this test is not to formally test random number generation, but rather to
+ casually check that "it works" and catch regressions: */
+
+ // Overload taking an int should compile:
+ ceph::util::randomize_rng(42);
+
+ // The default overload should compile:
+ ceph::util::randomize_rng();
+
+ {
+ int a = ceph::util::generate_random_number();
+ int b = ceph::util::generate_random_number();
+
+ // Technically, this can still collide and cause a false negative, but let's be optimistic:
+ if(std::numeric_limits<int>::max() > 32767)
+  {
+    ASSERT_GT(a, -1);
+    ASSERT_GT(b, -1);
+
+    ASSERT_NE(a, b);
+  }
+ }
+
+ {
+ auto a = ceph::util::generate_random_number(1, std::numeric_limits<int>::max());
+ auto b = ceph::util::generate_random_number(1, std::numeric_limits<int>::max());
+
+ if(std::numeric_limits<int>::max() > 32767)
+  {
+    ASSERT_GT(a, 0);
+    ASSERT_GT(b, 0);
+
+    ASSERT_NE(a, b);
+  }
+ }
+
+ for(auto n = 100000; n; --n)
+  {
+    constexpr int min = 0, max = 6;
+    int a = ceph::util::generate_random_number<min, max>();
+    ASSERT_GT(a, -1);
+    ASSERT_LT(a, 7);
+  }
+
+ // Multiple types (integral):
+ {
+ int min = 0, max = 1;
+ type_check_ok(min, max);
+ }
+
+ {
+ long min = 0, max = 1l;
+ type_check_ok(min, max);
+ }
+
+ // Multiple types (floating point):
+ {
+ double min = 0.0, max = 1.0;
+ type_check_ok(min, max);
+ }
+
+ {
+ float min = 0.0, max = 1.0;
+ type_check_ok(min, max);
+ }
+
+ // min > max should not explode:
+ {
+ float min = 1.0, max = 0.0;
+ auto x = ceph::util::generate_random_number(min, max);
+ }
+}
+
+TEST(util, test_user_mutex)
+{
+ // Show that we can call with a user lock:
+ std::mutex l;
+
+ ceph::util::randomize_rng(l);
+ ceph::util::randomize_rng(123, l);
+
+ ceph::util::generate_random_number(l);
+
+ ceph::util::generate_random_number(20, l);
+ ceph::util::generate_random_number(1, 20, l);
+ ceph::util::generate_random_number(20l, l);
+ ceph::util::generate_random_number(1l, 20l, l);
+
+ ceph::util::generate_random_number(20.0, l);
+ ceph::util::generate_random_number(1.0, 20.0, l);
+ ceph::util::generate_random_number(20.0d, l);
+ ceph::util::generate_random_number(1.0d, 20.0d, l);
+}
+
+TEST(util, test_random_user_rng_lock)
+{
+ /* Users may not want to use the static RNG-- for example,
+ in a case where the developer would like one RNG per thread. For
+ now, we support a custom RNG only along with an accompanying
+ custom lock through the function interface: */
+
+ std::mutex l;
+ std::mt19937_64 e;
+
+ ceph::util::randomize_rng(l, e);
+
+ ceph::util::randomize_rng(123, l, e);
+
+ ceph::util::generate_random_number(l, e);
+
+ ceph::util::generate_random_number(20, l, e);
+ ceph::util::generate_random_number(1, 20, l, e);
+
+ ceph::util::generate_random_number(20l, l, e);
+
+ ceph::util::generate_random_number(1l, 20l, l, e);
+
+ ceph::util::generate_random_number(20.0, l, e);
+ ceph::util::generate_random_number(1.0, 20.0, l, e);
+
+ ceph::util::generate_random_number(20.0d, l, e);
+ ceph::util::generate_random_number(1.0d, 20.0d, l, e);
+}
+
+TEST(util, test_make_random_function)
+{
+ // Test convenience functions:
+
+ constexpr auto max_int   = std::numeric_limits<int>::max();
+
+ {
+ auto rng_i = ceph::util::make_random_number_function<1, max_int - 1>();
+
+ int x = rng_i(), y = rng_i();
+
+ ASSERT_GT(x, 0);
+ ASSERT_LT(x, max_int);
+ ASSERT_GT(y, 0);
+ ASSERT_LT(y, max_int);
+ ASSERT_NE(x, y);
+ }
+ 
+ // User may specify engine:
+ {
+ auto rng_i = ceph::util::make_random_number_function<1, 10, std::mt19937_64>();
+ int x = rng_i();
+ ASSERT_GT(x, 0); 
+ ASSERT_LT(x, 10); 
+ }
+
+ // User specified seed:
+ {
+ auto rng_i = ceph::util::make_random_number_function<1, 10, 42>();
+ int x = rng_i();
+ ASSERT_GT(x, 0); 
+ ASSERT_LT(x, 10); 
+ }
+
+}
+
+TEST(util, test_random_class_interface)
+{
+ ceph::util::random_number_generator<int> rng;
+
+ // Other ctors:
+ {
+ ceph::util::random_number_generator<int> rng(1234);   // seed
+ }
+
+ {
+ auto a = rng();
+ auto b = rng();
+
+ // Technically can fail, but should "almost never" happen:
+ ASSERT_NE(a, b);
+ }
+
+ {
+ auto a = rng(10);
+ ASSERT_LT(a, 11);
+ ASSERT_GT(a, -1);
+ }
+
+ {
+ auto a = rng(10.0);
+ ASSERT_LT(a, 11.0);
+ ASSERT_GT(a, -1.0);
+ }
+
+ {
+ auto a = rng(10, 20);
+ ASSERT_LT(a, 21);
+ ASSERT_GT(a, 9);
+ }
+
+ {
+ auto a = rng(10.0, 20.0);
+ ASSERT_LT(a, 20.1);
+ ASSERT_GT(a, 9.9);
+ }
+}
+
